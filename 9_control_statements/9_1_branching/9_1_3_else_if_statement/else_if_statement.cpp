047bb9621c4ea7ab0a412b9012c959b99f26606f
Arshvir <avarshvir@gmail.com>
2024-06-10 21:02:02 +0530

    commit

---
+++ 9_control_statements/9_1_branching/9_1_3_else_if_statement/else_if_statement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1 = 15;
+    int num2 = 30;
+    int num3 = 10;
+    
+    if(num1 > num2 && num1 > num3){
+        cout << "num1 is greater";
+    }
+    else if (num2 > num1 && num2 > num3){
+        cout << "num2 is greater";
+    }
+    else{
+        cout << "num3 is greater";
+    }
+    
+}
---
+++ 9_control_statements/9_1_branching/9_1_4_switch_statement/switch_statement.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout << "enter character : ";
+    cin >> ch;
+    switch (ch)
+    {
+    case 'a':
+        cout << ch <<" is vowel";
+        break;
+    case 'e':
+        cout << ch <<" is vowel";
+        break;
+    case 'i':
+        cout << ch <<" is vowel";
+        break;
+    case 'o':
+        cout << ch <<" is vowel";
+        break;
+    case 'u':
+        cout << ch <<" is vowel";
+        break;
+    default:
+        cout << ch <<" is consonant";
+        break;
+    }
+}
