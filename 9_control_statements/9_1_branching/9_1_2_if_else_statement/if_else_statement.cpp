554ce257e999936154c9f81b6c20ab9c144a6ae1
Arshvir <avarshvir@gmail.com>
2024-06-10 20:15:41 +0530

    commit

---
+++ 9_control_statements/9_1_branching/9_1_2_if_else_statement/if_else_statement.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    num = 15;
+    if(num % 2 == 0){
+        cout << "number is even"<<endl;
+    }
+    else{
+        cout << "number is odd"<<endl;
+    }
+}
