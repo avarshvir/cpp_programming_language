dc599c4227005337ff493d8bc192d3f4f925d482
Arshvir <avarshvir@gmail.com>
2024-07-28 23:24:43 +0530

    commit

---
+++ modular_programming_and_seperate_compilation/a.cpp
@@ -0,0 +1,6 @@
+#include "a.h"
+#include <iostream>
+
+void functionInA() {
+    std::cout << "Hello from functionInA in a.cpp" << std::endl;
+}
---
+++ modular_programming_and_seperate_compilation/b.cpp
@@ -0,0 +1,6 @@
+#include "a.h"
+
+int main() {
+    functionInA();
+    return 0;
+}
