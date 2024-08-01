c7d9265e00db41b64b6f2bdd35500e8b06ef5f42
Arshvir <avarshvir@gmail.com>
2024-08-01 15:56:18 +0530

    commit

--- 18_structure,union_and_enum/18_1_structure/structure_1.cpp
+++ 18_structure,union_and_enum/18_1_structure/structure_1.cpp
@@ -4,9 +4,12 @@ struct Point
 {
     int a =10;
     int b;
+    
 };
 int main(){
     struct Point p1;
-    p1.b =20;
-    cout << p1.a << " " <<p1.b << endl;
+    
+    cout << p1.a << endl;
+
+ 
 }
