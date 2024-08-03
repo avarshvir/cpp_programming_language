0dce2428577c1c814a42f61973a5dae58fa75b73
Arshvir <avarshvir@gmail.com>
2024-08-03 22:03:55 +0530

    commit

---
+++ 19_object_oriented_programming/19_2_constructor/19_2_3_multiple_constructor.cpp/connstructor_1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class MultipleConstructor{
+    private:
+    int num_data;
+    float float_data;
+    string text_data;
+    bool binary_data;
+
+    public:
+    MultipleConstructor(){
+        cout << "This is Default Constructor " << endl;
+    }
+
+    MultipleConstructor(int num_data, bool binary_data){
+        this->num_data = num_data;
+        this->binary_data = binary_data;
+    }
+
+    MultipleConstructor(int num_data, float float_data, string text_data, bool binary_data){
+        this->num_data = num_data;
+        this->float_data = float_data;
+        this->text_data = text_data;
+        this->binary_data = binary_data;
+    }
+
+    void display(){
+        if(!float_data){
+            cout << "numeric data : " << num_data << endl;
+            cout << "binary data : " << binary_data << endl;
+        }else{
+            cout << "numeric data : " << num_data << endl;
+            cout << "float data : " << float_data << endl;
+            cout << "text data : " << text_data << endl;
+            cout << "binary data : " << binary_data << endl;
+        }
+       
+        
+        
+    }
+
+};
+
+int main(){
+    MultipleConstructor m1;
+    MultipleConstructor m2(10,0),m3(10,1.5,"hello",1);
+    m2.display();
+    m3.display();
+}
