5b83448dbb28646875a50a925bcba164ad2b4703
avarshvir <avarshvir@gmail.com>
2025-03-29 23:45:21 +0530

    static variable

--- 3_variables/static_variable_4.cpp
+++ 3_variables/static_variable_4.cpp
@@ -5,8 +5,19 @@ void staticVariable(){
     cout << count << " ";
     count++;
 }
+
+void counter(){
+    static int countt = 0;
+    countt++;
+    cout << "countt: " << countt << endl;
+}
+
 int main(){
     for(int i = 0; i < 5; i++)
         staticVariable();
+
+    cout << endl;
+    counter();
+    counter();
     return 0;
 }
